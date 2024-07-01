7e0ac2924faf023eafcd8cc14e7e9b4d0274545f
Soumya Ranjan Nayak <128832907+SoumyaNayak2000@users.noreply.github.com>
2024-07-01 16:20:22 +0530

    Update constants.js

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,1 +1,14 @@
 export const DB_NAME = "yt-manager"
+
+
+// PORT=8080
+// MONGODB_URI=mongodb+srv://soumyanayakraju:soumya123@users.bjkb9lj.mongodb.net
+// CORS_ORIGIN=*
+// ACCESS_TOKEN_SECRET=hellosecreat
+// ACCESS_TOKEN_EXPIRY=1d
+// REFRESH_TOKEN_SECRET=hellosecreattoken
+// REFRESH_TOKEN_EXPIRY=10d
+
+// CLOUDINARY_CLOUD_NAME=duxrghtw7
+// CLOUDINARY_API_KEY=755565957279871
+// CLOUDINARY_API_SECRET=KwVPENtx24SoD-aHXnHqTSxrPTY
